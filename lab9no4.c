4e99dd45ff770311317e812f8dea64e25f2109ce
6704062611221 <s6704062611221@email.kmutnb.ac.th>
2026-02-24 13:31:49 +0700

    swap array1d

--- lab9no4.c
+++ lab9no4.c
@@ -1,26 +1,38 @@
 #include<stdio.h>
-void swapValue(int *a, int *b, int *c);
-//void swapArray(int a[], int b[], int size); 
+//void swapValue(int *a, int *b, int *c);
+void swapArray(int a[], int b[], int size); 
 
 
 int main() {
-int a = 1, b = 2, c = 3;
-printf("Before swap function: a=%d, b=%d, c=%d\n", a, b, c);
-swapValue(&a,&b,&c);
-printf("After swap function: a= %d, b=%d, c=%d\n", a, b, c);
-
-
+	int a[3] = {1,2,3}, b[3] = {2,3,4}, c[3] = {3,4,5};
+	int i;
+	printf("Before Swap Array = a[3] = ");
+	for(i=0;i<3;i++){
+		printf("%d ",a[i]);
+	}
+	printf(" , b[3] = ");
+	for(i=0;i<3;i++){
+		printf(" %d",b[i]);
+	}
+	printf("\n");
+	swapArray(a,b,3);
 }
-void swapValue(int *a,int *b,int *c){
-	int swap;
-	swap = *a;
-	*a = *b;
-	*b = *c;
-	*c = swap;
-	
-	
+
+void swapArray(int a[],int b[],int size){
+	int i,swap;
+	for(i=0;i<size;i++){
+		swap = a[i];
+		a[i] = b[i];
+		b[i] = swap;
+	}
+	printf("After Swap Array : a[3] = ");
+	for(i=0;i<size;i++){
+		printf("%d ",a[i]);
+	}
+	printf(" , b[3] = ");
+	for(i=0;i<size;i++){
+		printf("%d ",b[i]);
+	}
+	printf("\n");
 }
-//void swapArray(int a[],int b[],int size){
-//	
-//}
 
